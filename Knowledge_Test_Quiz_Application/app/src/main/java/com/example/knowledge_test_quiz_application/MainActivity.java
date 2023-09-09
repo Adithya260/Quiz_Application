d3e00b62df7a932b2a679b30c575dff48ad35fe3
LENOVO <LENOVO@DESKTOP-S2L1QAE>
2023-09-09 11:01:38 +0530

    Am commiting the changes i done

---
+++ Knowledge_Test_Quiz_Application/app/src/main/java/com/example/knowledge_test_quiz_application/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.knowledge_test_quiz_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button startbutton= findViewById(R.id.button);
+        Button aboutbutton= findViewById(R.id.button2);
+        final EditText nametext=(EditText) findViewById(R.id.editName);
+
+             startbutton.setOnClickListener(new View.OnClickListener() {
+                 @Override
+                 public void onClick(View view) {
+                     String name=nametext.getText().toString();
+                     Intent intent=new Intent(getApplicationContext(),QuestionsActivity.class);
+                     intent.putExtra("myname",name);
+                     startActivity(intent);
+
+                 }
+             });
+             aboutbutton.setOnClickListener(new View.OnClickListener() {
+                 @Override
+                 public void onClick(View view) {
+                     Intent intent=new Intent(getApplicationContext(),DeveloperActivity.class);
+                     startActivity(intent);
+
+                 }
+             });
+
+    }
+}
